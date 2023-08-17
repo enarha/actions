82c51614051ecfecc1622e625cde8f061ad89663
Emil Natan <ena@redhat.com>
2023-08-17 12:32:29 +0300

    Changed listener port

--- app/main.go
+++ app/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8081", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
